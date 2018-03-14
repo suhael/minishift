36801a5160baffaf804eb638e688ff9d3adadeee
Suhael <suhael@giffgaff.co.uk>
2018-03-14 17:22:57 +0000

    add extra

--- src/main/java/com/suhael/minishift/controller/PersonController.java
+++ src/main/java/com/suhael/minishift/controller/PersonController.java
@@ -2,18 +2,17 @@ package com.suhael.minishift.controller;
 
 import com.suhael.minishift.model.Person;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+
 @RestController
-@RequestMapping("/person")
 public class PersonController {
 
-    @GetMapping
+    @RequestMapping("person")
     public List<Person> getPeople() {
         return Arrays.asList(
                 new Person("suhael", "akhtar"),
